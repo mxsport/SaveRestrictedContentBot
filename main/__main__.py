7b888b27c684e858f2f062bed5d3f8882db149c0
mxsport <126048612+mxsport@users.noreply.github.com>
2023-10-01 12:06:05 +0530

    Update __main__.py

--- main/__main__.py
+++ main/__main__.py
@@ -17,7 +17,7 @@ for name in files:
 
 #Don't be a thief 
 print("Successfully deployed!")
-print("By MaheshChauhan • DroneBots")
+print("By Akash Sodh • OTT Walla")
 
 if __name__ == "__main__":
     bot.run_until_disconnected()
