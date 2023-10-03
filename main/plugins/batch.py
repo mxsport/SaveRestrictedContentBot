165c9f3661dcfe76e7d0cd6dfc840bc42156aa77
mxsport <126048612+mxsport@users.noreply.github.com>
2023-10-03 12:11:38 +0530

    Update batch.py

--- main/plugins/batch.py
+++ main/plugins/batch.py
@@ -79,11 +79,11 @@ async def _batch(event):
 
 async def run_batch(userbot, client, sender, link, _range):
     for i in range(_range):
-        timer = 6
+        timer = 60
         if i < 25:
-            timer = 1
-        if i < 50 and i > 25:
             timer = 5
+        if i < 50 and i > 25:
+            timer = 10
         if i < 100 and i > 50:
             timer = 15
         if not 't.me/c/' in link:
